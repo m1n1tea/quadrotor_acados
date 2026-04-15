3353f4a37391dab126476cdf19ca5cb7aff8c006
Mikhail Makurov <m.makurov@yadro.com>
2026-04-15 11:37:48 +0300

    fix ned frame

--- src/controller.py
+++ src/controller.py
@@ -1,14 +1,26 @@
 import os
+
 import casadi as cs
 import numpy as np
-from acados_template import AcadosOcp, AcadosOcpSolver, AcadosModel
+from acados_template import AcadosModel, AcadosOcp, AcadosOcpSolver
+
 from quadrotor import Quadrotor3D
-from utils import skew_symmetric, v_dot_q, quaternion_inverse, transform_trajectory
+from utils import quaternion_inverse, skew_symmetric, transform_trajectory, v_dot_q
+
 
 class Controller:
-    def __init__(self, quad:Quadrotor3D, t_horizon=1, n_nodes=20,
-                 q_cost=None, r_cost=None, q_mask=None, rdrv_d_mat=None,
-                 model_name="quad_3d_acados_mpc", solver_options=None):
+    def __init__(
+        self,
+        quad: Quadrotor3D,
+        t_horizon=1,
+        n_nodes=20,
+        q_cost=None,
+        r_cost=None,
+        q_mask=None,
+        rdrv_d_mat=None,
+        model_name="quad_3d_acados_mpc",
+        solver_options=None,
+    ):
         """
         :param quad: quadrotor object
         :type quad: Quadrotor3D
@@ -25,7 +37,9 @@ class Controller:
 
         # Weighted squared error loss function q = (p_xyz, a_xyz, v_xyz, r_xyz), r = (u1, u2, u3, u4)
         if q_cost is None:
-            q_cost = np.array([10, 10, 10, 0.05, 0.05, 0.05, 0.05, 0.1, 0.1, 0.1, 0.05, 0.05])
+            q_cost = np.array(
+                [10, 10, 10, 0.05, 0.05, 0.05, 0.05, 0.1, 0.1, 0.1, 0.05, 0.05]
+            )
         if r_cost is None:
             r_cost = np.array([0.1, 0.1, 0.1, 0.1])
 
@@ -38,20 +52,20 @@ class Controller:
         self.min_u = quad.min_input_value
 
         # Declare model variables
-        self.p = cs.MX.sym('p', 3)  # position
-        self.q = cs.MX.sym('a', 4)  # angle quaternion (wxyz)
-        self.v = cs.MX.sym('v', 3)  # velocity
-        self.r = cs.MX.sym('r', 3)  # angle rate
+        self.p = cs.MX.sym("p", 3)  # position
+        self.q = cs.MX.sym("a", 4)  # angle quaternion (wxyz)
+        self.v = cs.MX.sym("v", 3)  # velocity
+        self.r = cs.MX.sym("r", 3)  # angle rate
 
         # Full state vector (13-dimensional)
         self.x = cs.vertcat(self.p, self.q, self.v, self.r)
         self.state_dim = 13
 
         # Control input vector
-        u1 = cs.MX.sym('u1')
-        u2 = cs.MX.sym('u2')
-        u3 = cs.MX.sym('u3')
-        u4 = cs.MX.sym('u4')
+        u1 = cs.MX.sym("u1")
+        u2 = cs.MX.sym("u2")
+        u3 = cs.MX.sym("u3")
+        u4 = cs.MX.sym("u4")
         self.u = cs.vertcat(u1, u2, u3, u4)
 
         # Nominal model equations symbolic function (no GP)
@@ -65,19 +79,24 @@ class Controller:
         # should be used to evaluate the dynamics function. It corresponds to self.x if there are no GP's, or
         # self.x_with_gp otherwise
         acados_models, nominal_with_gp = self.acados_setup_model(
-            self.quad_xdot_nominal(x=self.x, u=self.u)['x_dot'], model_name)
+            self.quad_xdot_nominal(x=self.x, u=self.u)["x_dot"], model_name
+        )
 
         # Convert dynamics variables to functions of the state and input vectors
         self.quad_xdot = {}
         for dyn_model_idx in nominal_with_gp.keys():
             dyn = nominal_with_gp[dyn_model_idx]
-            self.quad_xdot[dyn_model_idx] = cs.Function('x_dot', [self.x, self.u], [dyn], ['x', 'u'], ['x_dot'])
+            self.quad_xdot[dyn_model_idx] = cs.Function(
+                "x_dot", [self.x, self.u], [dyn], ["x", "u"], ["x_dot"]
+            )
 
         # ### Setup and compile Acados OCP solvers ### #
         self.acados_ocp_solver = None
 
         # Add one more weight to the rotation (use quaternion norm weighting in acados)
-        q_diagonal = np.concatenate((q_cost[:3], np.mean(q_cost[3:6])[np.newaxis], q_cost[3:]))
+        q_diagonal = np.concatenate(
+            (q_cost[:3], np.mean(q_cost[3:6])[np.newaxis], q_cost[3:])
+        )
         if q_mask is not None:
             q_mask = np.concatenate((q_mask[:3], np.zeros(1), q_mask[3:]))
             q_diagonal *= q_mask
@@ -98,12 +117,16 @@ class Controller:
             ocp.dims.np = n_param
             ocp.parameter_values = np.zeros(n_param)
 
-            ocp.cost.cost_type = 'LINEAR_LS'
-            ocp.cost.cost_type_e = 'LINEAR_LS'
+            ocp.cost.cost_type = "LINEAR_LS"
+            ocp.cost.cost_type_e = "LINEAR_LS"
 
             ocp.cost.W = np.diag(np.concatenate((q_diagonal, r_cost)))
             ocp.cost.W_e = np.diag(q_diagonal)
-            terminal_cost = 0 if solver_options is None or not solver_options["terminal_cost"] else 1
+            terminal_cost = (
+                0
+                if solver_options is None or not solver_options["terminal_cost"]
+                else 1
+            )
             ocp.cost.W_e *= terminal_cost
 
             ocp.cost.Vx = np.zeros((ny, nx))
@@ -127,14 +150,16 @@ class Controller:
             ocp.constraints.idxbu = np.array([0, 1, 2, 3])
 
             # Solver options
-            ocp.solver_options.qp_solver = 'FULL_CONDENSING_HPIPM'
-            ocp.solver_options.hessian_approx = 'GAUSS_NEWTON'
-            ocp.solver_options.integrator_type = 'ERK'
+            ocp.solver_options.qp_solver = "FULL_CONDENSING_HPIPM"
+            ocp.solver_options.hessian_approx = "GAUSS_NEWTON"
+            ocp.solver_options.integrator_type = "ERK"
             ocp.solver_options.print_level = 0
-            ocp.solver_options.nlp_solver_type = 'SQP_RTI' if solver_options is None else solver_options["solver_type"]
+            ocp.solver_options.nlp_solver_type = (
+                "SQP_RTI" if solver_options is None else solver_options["solver_type"]
+            )
 
             # Compile acados OCP solver if necessary
-            json_file = os.path.join('./', key_model.name + '_acados_ocp.json')
+            json_file = os.path.join("./", key_model.name + "_acados_ocp.json")
             self.acados_ocp_solver = AcadosOcpSolver(ocp, json_file=json_file)
 
     def acados_setup_model(self, nominal, model_name):
@@ -151,7 +176,7 @@ class Controller:
 
         def fill_in_acados_model(x, u, p, dynamics, name):
 
-            x_dot = cs.MX.sym('x_dot', dynamics.shape)
+            x_dot = cs.MX.sym("x_dot", dynamics.shape)
             f_impl = x_dot - dynamics
 
             # Dynamics model
@@ -175,7 +200,9 @@ class Controller:
         x_ = self.x
         dynamics_ = nominal
 
-        acados_models[0] = fill_in_acados_model(x=x_, u=self.u, p=[], dynamics=dynamics_, name=model_name)
+        acados_models[0] = fill_in_acados_model(
+            x=x_, u=self.u, p=[], dynamics=dynamics_, name=model_name
+        )
 
         return acados_models, dynamics_equations
 
@@ -193,8 +220,15 @@ class Controller:
         (6x1)
         """
 
-        x_dot = cs.vertcat(self.p_dynamics(), self.q_dynamics(), self.v_dynamics(rdrv_d), self.w_dynamics())
-        return cs.Function('x_dot', [self.x[:13], self.u], [x_dot], ['x', 'u'], ['x_dot'])
+        x_dot = cs.vertcat(
+            self.p_dynamics(),
+            self.q_dynamics(),
+            self.v_dynamics(rdrv_d),
+            self.w_dynamics(),
+        )
+        return cs.Function(
+            "x_dot", [self.x[:13], self.u], [x_dot], ["x", "u"], ["x_dot"]
+        )
 
     def p_dynamics(self):
         return self.v
@@ -210,7 +244,12 @@ class Controller:
 
         f_thrust = self.u * self.quad.max_thrust
         g = cs.vertcat(0.0, 0.0, -9.81)
-        a_thrust = cs.vertcat(0.0, 0.0, - (f_thrust[0] + f_thrust[1] + f_thrust[2] + f_thrust[3])) / self.quad.mass
+        a_thrust = (
+            cs.vertcat(
+                0.0, 0.0, -(f_thrust[0] + f_thrust[1] + f_thrust[2] + f_thrust[3])
+            )
+            / self.quad.mass
+        )
 
         v_dynamics = v_dot_q(a_thrust, self.q) + g
 
@@ -229,25 +268,39 @@ class Controller:
         x_f = cs.MX(self.quad.x_f)
         c_f = cs.MX(self.quad.z_l_tau)
         return cs.vertcat(
-            (-cs.mtimes(f_thrust.T, y_f) + (self.quad.J[1] - self.quad.J[2]) * self.r[1] * self.r[2]) / self.quad.J[0],
-            (cs.mtimes(f_thrust.T, x_f) + (self.quad.J[2] - self.quad.J[0]) * self.r[2] * self.r[0]) / self.quad.J[1],
-            (-cs.mtimes(f_thrust.T, c_f) + (self.quad.J[0] - self.quad.J[1]) * self.r[0] * self.r[1]) / self.quad.J[2])
-    
-    def update_trajectory(self, trajectory, preferred_speed = None):
+            (
+                cs.mtimes(f_thrust.T, y_f)
+                + (self.quad.J[1] - self.quad.J[2]) * self.r[1] * self.r[2]
+            )
+            / self.quad.J[0],
+            (
+                -cs.mtimes(f_thrust.T, x_f)
+                + (self.quad.J[2] - self.quad.J[0]) * self.r[2] * self.r[0]
+            )
+            / self.quad.J[1],
+            (
+                cs.mtimes(f_thrust.T, c_f)
+                + (self.quad.J[0] - self.quad.J[1]) * self.r[0] * self.r[1]
+            )
+            / self.quad.J[2],
+        )
+
+    def update_trajectory(self, trajectory, preferred_speed=None):
         """
         Sets trajectory for the controlled object to pursue
 
-        :param trajectory: 2d numpy array of (N+1) elements with size 3 [x,y,z] 
+        :param trajectory: 2d numpy array of (N+1) elements with size 3 [x,y,z]
         """
         if preferred_speed == None:
             self.time_traj = trajectory
         else:
-            self.time_traj = transform_trajectory(trajectory, preferred_speed * self.T / self.N)
+            self.time_traj = transform_trajectory(
+                trajectory, preferred_speed * self.T / self.N
+            )
         print("preferred distance between points:", preferred_speed * self.T / self.N)
         print("len(self.time_traj):", len(self.time_traj))
         print("first ten points:", self.time_traj[:10])
         self.last_closest_index = 0
-        
 
     def run_optimization(self, initial_state=None):
         """
@@ -269,22 +322,71 @@ class Controller:
         x_init = np.stack(x_init)
 
         # Set initial condition, equality constraint
-        self.acados_ocp_solver.set(0, 'lbx', x_init)
-        self.acados_ocp_solver.set(0, 'ubx', x_init)
+        self.acados_ocp_solver.set(0, "lbx", x_init)
+        self.acados_ocp_solver.set(0, "ubx", x_init)
 
-        starting_index = np.argmin(np.sum((self.time_traj[self.last_closest_index:] - initial_state[:3])**2, axis=1)) + self.last_closest_index
-        
-        local_trajectory = self.time_traj[starting_index:starting_index + self.N + 1]
+        starting_index = (
+            np.argmin(
+                np.sum(
+                    (self.time_traj[self.last_closest_index :] - initial_state[:3])
+                    ** 2,
+                    axis=1,
+                )
+            )
+            + self.last_closest_index
+        )
+
+        local_trajectory = self.time_traj[starting_index : starting_index + self.N + 1]
         if len(local_trajectory) < self.N + 1:
-            local_trajectory = np.pad(local_trajectory, ((0,self.N + 1 - len(local_trajectory)), (0, 0)), 'edge')
+            local_trajectory = np.pad(
+                local_trajectory,
+                ((0, self.N + 1 - len(local_trajectory)), (0, 0)),
+                "edge",
+            )
         self.last_closest_index = starting_index
 
         for j in range(self.N):
-            y_ref = np.array([local_trajectory[j,0], local_trajectory[j,1], local_trajectory[j,2], 1,0,0,0, 0,0,0, 0,0,0, 0,0,0,0])
-            self.acados_ocp_solver.set(j, 'yref', y_ref)
+            y_ref = np.array(
+                [
+                    local_trajectory[j, 0],
+                    local_trajectory[j, 1],
+                    local_trajectory[j, 2],
+                    1,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                ]
+            )
+            self.acados_ocp_solver.set(j, "yref", y_ref)
 
-        y_refN = np.array([local_trajectory[self.N,0], local_trajectory[self.N,1], local_trajectory[self.N,2], 1,0,0,0, 0,0,0, 0,0,0])
-        self.acados_ocp_solver.set(self.N, 'yref', y_refN)
+        y_refN = np.array(
+            [
+                local_trajectory[self.N, 0],
+                local_trajectory[self.N, 1],
+                local_trajectory[self.N, 2],
+                1,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+            ]
+        )
+        self.acados_ocp_solver.set(self.N, "yref", y_refN)
 
         # Solve OCP
         self.acados_ocp_solver.solve()
--- src/main.py
+++ src/main.py
@@ -1,30 +1,31 @@
-import numpy as np
 import math
 import timeit
+
 import matplotlib
-matplotlib.use('Qt5Agg')
+import numpy as np
+
+matplotlib.use("Qt5Agg")
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
-from mpl_toolkits.mplot3d import Axes3D 
-
-from quadrotor import Quadrotor3D
 from controller import Controller
 from pid_controller import PIDController
-
+from quadrotor import Quadrotor3D
 from utils import quaternion_to_euler, transform_trajectory
+
 
 def createTrajectory(points_count):
     xref = []
     yref = []
     zref = []
     radius = 2.0
-    height = 3.0 
+    height = -3.0
     offset_height = 0  # Offset height
     num_turns = 0
     x_offset = 1
     y_offset = 0.5
     for i in range(points_count):
-        t = i / (points_count-1)
+        t = i / (points_count - 1)
         x = radius * math.cos(2 * math.pi * num_turns * t) + x_offset * t - radius
         y = radius * math.sin(2 * math.pi * num_turns * t) + y_offset * t
         z = height * t + offset_height  # Add offset height
@@ -33,36 +34,44 @@ def createTrajectory(points_count):
         zref.append(z)
     return np.array([np.array(xref), np.array(yref), np.array(zref)]).T
 
-def trackTrajectory(trajectory, preferred_speed = 0.5, noise = False, controller_type="Acados", ):
-    sim_time = 7 # Simulation time
-    N = 20       # Horizontal length
-    
-    quad = Quadrotor3D(motor_noise=noise)    # Quadrotor model
+
+def trackTrajectory(
+    trajectory,
+    preferred_speed=0.5,
+    noise=False,
+    controller_type="Acados",
+):
+    sim_time = 7  # Simulation time
+    N = 20  # Horizontal length
+
+    quad = Quadrotor3D(motor_noise=noise)  # Quadrotor model
     controller = None
-    if(controller_type == "Acados"):
-        dt = 0.1      # Time step
-        controller = Controller(quad, t_horizon=2*N*dt, n_nodes=N)  # Initialize MPC controller
-    elif(controller_type == "PID"):
-        dt = 0.001      # Time step
-        controller = PIDController(quad, dt = dt)  # Initialize PID controller
+    if controller_type == "Acados":
+        dt = 0.1  # Time step
+        controller = Controller(
+            quad, t_horizon=2 * N * dt, n_nodes=N
+        )  # Initialize MPC controller
+    elif controller_type == "PID":
+        dt = 0.001  # Time step
+        controller = PIDController(quad, dt=dt)  # Initialize PID controller
     else:
         print("Invalid controller type")
         return
-    xref = trajectory[:,0]
-    yref = trajectory[:,1]
-    zref = trajectory[:,2]
-    #xref, yref, zref = createTrajectory(10)
+    xref = trajectory[:, 0]
+    yref = trajectory[:, 1]
+    zref = trajectory[:, 2]
+    # xref, yref, zref = createTrajectory(10)
     trajectory = np.array([xref, yref, zref]).T
     controller.update_trajectory(trajectory, preferred_speed=preferred_speed)
     path = []
     q_path = []
     u_path = []
 
-    #xref_cmp, yref_cmp, zref_cmp = createTrajectory(int(sim_time/dt))
+    # xref_cmp, yref_cmp, zref_cmp = createTrajectory(int(sim_time/dt))
 
     # Main loop
     time_record = []
-    for i in range(int(sim_time/dt)):
+    for i in range(int(sim_time / dt)):
         current = np.concatenate([quad.pos, quad.angle, quad.vel, quad.a_rate])
         start = timeit.default_timer()
         thrust = controller.run_optimization(initial_state=current)[:4]
@@ -71,26 +80,26 @@ def trackTrajectory(trajectory, preferred_speed = 0.5, noise = False, controller
         path.append(quad.pos)
         q_path.append(quad.angle)
         u_path.append(thrust)
-    
+
     # CPU time
     print("average estimation time is {:.5f}".format(np.array(time_record).mean()))
     print("max estimation time is {:.5f}".format(np.array(time_record).max()))
     print("min estimation time is {:.5f}".format(np.array(time_record).min()))
 
     # reference_trajectory = np.array([xref_cmp, yref_cmp, zref_cmp]).T
-    
+
     # # Приводим длины массивов к одинаковому размеру (на случай расхождений)
     # min_len = min(len(path), len(reference_trajectory))
     # path_trimmed = np.array(path)[:min_len]
     # ref_trimmed = reference_trajectory[:min_len]
-    
+
     # # Вычисляем Евклидово расстояние для каждого шага времени
     # deviations = np.sqrt(np.sum((path_trimmed - ref_trimmed)**2, axis=1))
-    
+
     # # Максимальное и среднее отклонение
     # max_deviation = np.max(deviations)
     # mean_deviation = np.mean(deviations)
-    
+
     # print(f"Maximum deviation from path: {max_deviation:.4f} m")
     # print(f"Average deviation from path: {mean_deviation:.4f} m")
 
@@ -101,52 +110,52 @@ def trackTrajectory(trajectory, preferred_speed = 0.5, noise = False, controller
     plt.figure()
     plt.plot(time_record)
     plt.legend()
-    plt.ylabel('CPU Time [s]')
+    plt.ylabel("CPU Time [s]")
     # plt.yscale("log")
 
     # Visualize inputs
     u_path = np.array(u_path)
-    time = np.arange(0, len(u_path)*dt, dt)
+    time = np.arange(0, len(u_path) * dt, dt)
     plt.figure()
     plt.suptitle("Rotor thrust - normalized")
     plt.subplot(2, 2, 1)
     plt.plot(time, u_path[:, 0])
-    plt.ylabel('u1')
-    plt.xlabel('Time [s]')
+    plt.ylabel("u1")
+    plt.xlabel("Time [s]")
     plt.subplot(2, 2, 2)
     plt.plot(time, u_path[:, 1])
-    plt.ylabel('u2')
-    plt.xlabel('Time [s]')
+    plt.ylabel("u2")
+    plt.xlabel("Time [s]")
     plt.subplot(2, 2, 3)
     plt.plot(time, u_path[:, 2])
-    plt.ylabel('u3')
-    plt.xlabel('Time [s]')
+    plt.ylabel("u3")
+    plt.xlabel("Time [s]")
     plt.subplot(2, 2, 4)
     plt.plot(time, u_path[:, 3])
-    plt.ylabel('u4')
-    plt.xlabel('Time [s]')
+    plt.ylabel("u4")
+    plt.xlabel("Time [s]")
     plt.tight_layout()
-    
+
     # Visualize quaternion
     q_path = np.array(q_path)
     plt.figure()
     plt.suptitle("UAV attitude")
     plt.subplot(2, 2, 1)
     plt.plot(time, q_path[:, 0])
-    plt.ylabel('qw')
-    plt.xlabel('Time [s]')
+    plt.ylabel("qw")
+    plt.xlabel("Time [s]")
     plt.subplot(2, 2, 2)
     plt.plot(time, q_path[:, 1])
-    plt.ylabel('qx')
-    plt.xlabel('Time [s]')
+    plt.ylabel("qx")
+    plt.xlabel("Time [s]")
     plt.subplot(2, 2, 3)
     plt.plot(time, q_path[:, 2])
-    plt.ylabel('qy')
-    plt.xlabel('Time [s]')
+    plt.ylabel("qy")
+    plt.xlabel("Time [s]")
     plt.subplot(2, 2, 4)
     plt.plot(time, q_path[:, 3])
-    plt.ylabel('qz')
-    plt.xlabel('Time [s]')
+    plt.ylabel("qz")
+    plt.xlabel("Time [s]")
     plt.tight_layout()
 
     # q_path = np.array(q_path)
@@ -168,27 +177,46 @@ def trackTrajectory(trajectory, preferred_speed = 0.5, noise = False, controller
     # plt.ylabel('Z')
     # plt.xlabel('Time [s]')
     # plt.tight_layout()
-    
-    
+
     return path
+
 
 if __name__ == "__main__":
     # move2Goal()
     gt_traj = createTrajectory(100)
     pid_traj = trackTrajectory(createTrajectory(2), controller_type="PID")
-    acados_traj = trackTrajectory(createTrajectory(10), controller_type="Acados", noise=True, preferred_speed=0.9)
+    acados_traj = trackTrajectory(
+        createTrajectory(10), controller_type="Acados", noise=True, preferred_speed=0.9
+    )
     print(gt_traj)
 
-
     plt.figure()
-    plt.title('Визуализация траекторий дрона')
-    ax = plt.axes(projection='3d')
-    ax.plot(gt_traj[:,0], gt_traj[:,1], gt_traj[:,2], c=[0,0.7,0], label='Референсная траектория')
-    ax.plot(acados_traj[:,0], acados_traj[:,1], acados_traj[:,2], c=[0,0,1], label='Решение Acados контроллера')
-    ax.plot(pid_traj[:,0], pid_traj[:,1], pid_traj[:,2], c=[1,0,0], label='Решение PID контроллера')
-    ax.axis('auto')
-    ax.set_xlabel('x [m]')
-    ax.set_ylabel('y [m]')
-    ax.set_zlabel('z [m]') 
+    plt.title("Визуализация траекторий дрона")
+    ax = plt.axes(projection="3d")
+    ax.plot(
+        gt_traj[:, 0],
+        gt_traj[:, 1],
+        gt_traj[:, 2],
+        c=[0, 0.7, 0],
+        label="Референсная траектория",
+    )
+    ax.plot(
+        acados_traj[:, 0],
+        acados_traj[:, 1],
+        acados_traj[:, 2],
+        c=[0, 0, 1],
+        label="Решение Acados контроллера",
+    )
+    ax.plot(
+        pid_traj[:, 0],
+        pid_traj[:, 1],
+        pid_traj[:, 2],
+        c=[1, 0, 0],
+        label="Решение PID контроллера",
+    )
+    ax.axis("auto")
+    ax.set_xlabel("x [m]")
+    ax.set_ylabel("y [m]")
+    ax.set_zlabel("z [m]")
     ax.legend()
     plt.show()
